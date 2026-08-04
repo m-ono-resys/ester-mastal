d53923a90717687b683eaf5e85d0b9328b4b3f5a
Masatoshi Ono <m-ono@resys.jp>
2026-08-04 16:27:31 +0900

    アイテムのデータ構造をリストから辞書に変更し、初期プレイヤーにアイテムを追加

--- src/ester_mastal/models/repository.py
+++ src/ester_mastal/models/repository.py
@@ -75,6 +75,7 @@ class GameRepository:
             level=1,
             exp=0,
             gold=0,
+            items=[{"herb": 2}],
         )
 
     def check_level_up(self, player: Player) -> list[str]:
