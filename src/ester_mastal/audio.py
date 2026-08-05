b83108c7ebad06a270ba5bdc96e8eeb74baf9d99
Masatoshi Ono <m-ono@resys.jp>
2026-08-05 16:06:04 +0900

    効果音の設定を修正し、pyxel.soundからpyxel.soundsに変更

--- src/ester_mastal/audio.py
+++ src/ester_mastal/audio.py
@@ -4,16 +4,16 @@ import pyxel
 def init_audio():
     """コード上でレトロ効果音とファンファーレを自動生成"""
     # SE 0: 攻撃音（シュッと下降する音）
-    pyxel.sound(0).set("c3c2", "n", "7", "f", 4)
+    pyxel.sounds[0].set("c3c2", "n", "7", "f", 4)
 
     # SE 1: 回復魔法音（ポピポピ音）
-    pyxel.sound(1).set("g3c4e4g4", "t", "6", "v", 8)
+    pyxel.sounds[1].set("g3c4e4g4", "t", "6", "v", 8)
 
     # SE 2: ダメージ音（ドゴッという重低音）
-    pyxel.sound(2).set("f2c2", "p", "7", "f", 6)
+    pyxel.sounds[2].set("f2c2", "p", "7", "f", 6)
 
     # SE 3: 勝利ファンファーレ（タタタターン！）
-    pyxel.sound(3).set("c3e3g3c4 c4c4 g3c4", "t", "6", "v", 12)
+    pyxel.sounds[3].set("c3e3g3c4 c4c4 g3c4", "t", "6", "v", 12)
 
 
 def play_se(sound_id: int):
