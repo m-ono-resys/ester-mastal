b87979dc049cf1c04885da7a5f171d00b80a471b
Masatoshi Ono <m-ono@resys.jp>
2026-08-14 01:41:56 +0900

    モンスターリポジトリのfind_by_codeメソッドを修正し、独立したコピーを返すように変更

--- src/ester_mastal/infrastructure/in_memory_monster_repository.py
+++ src/ester_mastal/infrastructure/in_memory_monster_repository.py
@@ -1,3 +1,5 @@
+import copy
+
 from ..data.monsters import MONSTER_MASTER
 from ..models.monster import Monster, MonsterCode, MonsterRepository
 
@@ -7,7 +9,11 @@ class InMemoryMonsterRepository(MonsterRepository):
         self._master_data = master_data
 
     def find_by_code(self, code):
-        return self._master_data.get(code)
+        monster = self._master_data.get(code)
+        if monster is not None:
+            # ★ copy.deepcopy() で原本を変更させない「独立したコピー」を返します！
+            return copy.deepcopy(monster)
+        return None
 
     def get_all(self):
         return list(self._master_data.values())
